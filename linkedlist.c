ffed7be36b8f1772626e8622abae6751f6982140
ganeshprasad21 <ganesh21111999@gmail.com>
2020-07-12 21:19:05 +0530

    Update linkedlist.c

--- linkedlist.c
+++ linkedlist.c
@@ -10,17 +10,26 @@ void create(node * first);
 void displayit(node * first);
 void recursivedisp(node * first);
 void recursivereverse(node * first);
+node * insert(node * first,int posn, int ele);
+node * insert_to_a_sorted_linked_list(node * start,int ele);
 int main() {
     node * start;
-    
     start = (node *)malloc(sizeof(node));
     printf("enter the elements\n\n");
     create(start); 
-    printf("wait\n\n");
+    printf("wait \n\n");
     displayit(start);
     recursivedisp(start);
     printf("recursive-reverse\n\n");
     recursivereverse(start);
+    start = insert(start,0,0);
+    displayit(start);
+    start = insert(start,1,2);
+    printf("---");
+    displayit(start);
+    printf("hi");
+    start = insert_to_a_sorted_linked_list(start,3);
+    displayit(start);
     return (EXIT_SUCCESS);
 }
 void create(node * first){
@@ -65,5 +74,34 @@ void recursivereverse(node * first)
                 printf("%d",first->data);
     }
 }
-
-
+node * insert(node * first,int posn, int ele){
+    node * follow_f; //follow fast follow slow
+    if (posn == 0)
+    {
+    follow_f = (node *)malloc(sizeof(node));
+    follow_f->data = ele;
+    follow_f->next = first;
+    first = follow_f;return first;
+    }
+    else{
+        node * first_1 = first;
+        node * value = (node *)malloc(sizeof(node));
+        value->data = ele;
+        for(int i=0;i<posn-1;i++){
+            first=first->next; 
+        }
+        value->next = first->next;
+        first->next = value;
+        return first_1;
+    }
+}
+node * insert_to_a_sorted_linked_list(node * start, int ele){
+    int value = 0;
+    node * val_1 = start;
+    while(start->next != NULL){
+        if (start->data < ele)
+                value += 1;
+        start = start->next;
+        
+}return insert(val_1,value,ele);
+}
